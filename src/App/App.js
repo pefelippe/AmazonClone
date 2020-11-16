2b1beb909d3090ff2c74a44448daa231f0268b40
Pedro Felippe <pedfelippe@gmail.com>
2020-11-15 22:08:39 -0300

    feat: initial commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBT0R8t33anItfe0ZYinN4QiUH2ny8vdHQ",
+    authDomain: "clone-617cc.firebaseapp.com",
+    databaseURL: "https://clone-617cc.firebaseio.com",
+    projectId: "clone-617cc",
+    storageBucket: "clone-617cc.appspot.com",
+    messagingSenderId: "302267729678",
+    appId: "1:302267729678:web:3d4b94626cfeeaf568b95f"
+  };
