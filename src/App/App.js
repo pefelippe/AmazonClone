bfe6232a069df49c4d4219104ead339972674c3f
Pedro Felippe <pedfelippe@gmail.com>
2020-11-17 01:12:49 -0300

    feat: home page completed

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
+import Home from "../Home/Home";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,38 +2,42 @@ import React from "react";
 
 import "./Header.css";
 
+import SearchIcon from "@material-ui/icons/Search";
+
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
 function Header() {
   return (
     <div className="header">
       <img
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
         className="header__logo"
       />
+
       <div className="header__search">
-        <input type="text" className="header__seatchIn" />
-        {/* LOGO */}
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
+
       <div className="header__nav">
         <div className="header__options">
           <span className="header__optionLineOne">Hello, $[user]</span>
-        </div>
-        <div className="header__options">
           <span className="header__optionLineTwo">Sign In</span>
         </div>
 
         <div className="header__options">
           <span className="header__optionLineOne">Returns</span>
-        </div>
-        <div className="header__options">
           <span className="header__optionLineTwo">Orders</span>
         </div>
 
         <div className="header__options">
           <span className="header__optionLineOne">Your</span>
-        </div>
-        <div className="header__options">
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
